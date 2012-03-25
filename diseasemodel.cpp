AdamMajer/bshouty-lung-model

commit 2203406eadb3916e5d2f6e37f8875515c77d480a

Added Range capability to disease parameters
Disease parameters are set via Model::setData() and Model::getData()
functions where DataType is OR'ed with disease index and param
index. See Model::diseaseHybridType() for details.

  * Range edit action moved inside ClickableLineEdit
  * DiseaseModel holds range values for each parameter
  * multi-results output handles disease range parameters

diseasemodel.cpp
@@ -21,6 +21,8 @@
 #include "diseasemodel.h"
 #include <algorithm>
 #include <stdexcept>
+#include <QSqlError>
+#include <QSqlQuery>
 
 #define HEADER_ID 0xFFFF
 
@@ -50,9 +52,20 @@ DiseaseModel::DiseaseModel(DiseaseList &diseases, QObject *parent)
         : QAbstractItemModel(parent)
 {
 	disease_list.reserve(diseases.size());
+	disease_param_ranges.reserve(diseases.size());
 
-	for(DiseaseList::const_iterator i=diseases.begin(); i!=diseases.end(); ++i)
+	for(DiseaseList::const_iterator i=diseases.begin(); i!=diseases.end(); ++i) {
 		disease_list.push_back(*i);
+
+		const int n_params = i->paramCount();
+		std::vector<Range> params;
+		params.reserve(n_params);
+
+		for (int param_no=0; param_no<n_params; ++param_no)
+			params.push_back(Range(QString::number(i->parameterValue(param_no), 'f')));
+
+		disease_param_ranges.push_back(params);
+	}
 }
 
 DiseaseModel::~DiseaseModel()
@@ -101,8 +114,7 @@ QVariant DiseaseModel::data(const QModelIndex &index, int role) const
 	case Qt::BackgroundRole:
 		if (diseaseParamNo(index.internalId()) == HEADER_ID)
 			return QBrush(QColor(0xa0, 0xa0, 0xff));
-		if (disease_index.row() == index.row() &&
-		    disease_index.internalId() == index.internalId())
+		if (disease_index.internalId() == index.internalId())
 			return QBrush(QColor(0xff, 0xff, 0xa0));
 		break;
 	case Qt::DisplayRole:
@@ -120,8 +132,7 @@ QVariant DiseaseModel::data(const QModelIndex &index, int role) const
 
 			if (index.column() == 0)
 				return d.parameterName(param_no);
-			else
-				return d.parameterValue(param_no);
+			return disease_param_ranges.at(disease_no).at(param_no).toString();
 		}
 		break;
 	}
@@ -149,11 +160,16 @@ bool DiseaseModel::setData(const QModelIndex &index, const QVariant &value, int 
 		QAbstractItemModel::setData(index, value, role);
 
 	switch (role) {
-	case Qt::EditRole:
-		disease_list[index.parent().row()].setParameter(index.row(), value.toDouble());
+	case Qt::EditRole: {
+		const int disease_no = diseaseNo(index.internalId());
+		const int param_no = diseaseParamNo(index.internalId());
+		Range range(value.toString());
+		disease_list[disease_no].setParameter(index.row(), range.firstValue());
+		disease_param_ranges[disease_no][param_no] = range;
 		emit dataChanged(index, index);
 		return true;
-	}
+	} // Qt::EditRole
+	} // switch
 
 	return false;
 }
@@ -192,6 +208,14 @@ void DiseaseModel::addDisease(const Disease &d)
 
 	beginInsertRows(root_node, size, size);
 	disease_list.push_back(d);
+
+	const int param_count = d.paramCount();
+	std::vector<Range> params;
+	params.reserve(param_count);
+	for (int i=0; i<param_count; ++i)
+		params.push_back(Range(QString::number(d.parameterValue(i), 'f')));
+
+	disease_param_ranges.push_back(params);
 	endInsertRows();
 
 	emit diseaseAdded(index(size, 0, root_node));
@@ -208,6 +232,7 @@ void DiseaseModel::removeDisease(const Disease &d)
 		if (disease_list[row].id()== d.id()) {
 			beginRemoveRows(root_node, row, row);
 			disease_list.erase(disease_list.begin() + row);
+			disease_param_ranges.erase(disease_param_ranges.begin() + row);
 
 			if (disease_index.isValid()) {
 				int dis_no = diseaseNo(disease_index.internalId());
@@ -234,6 +259,7 @@ void DiseaseModel::clear()
 	beginResetModel();
 	disease_list.clear();
 	disease_index = QModelIndex();
+	disease_param_ranges.clear();
 	endResetModel();
 }
 
@@ -249,12 +275,36 @@ DiseaseList DiseaseModel::diseases() const
 	return ret;
 }
 
+std::vector<std::vector<Range> > DiseaseModel::diseaseParamRanges() const
+{
+	return disease_param_ranges;
+}
+
 void DiseaseModel::setDiseases(const DiseaseList &list)
+{
+	beginResetModel();
+	disease_list.clear();
+	disease_param_ranges.clear();
+	for (DiseaseList::const_iterator i=list.begin(); i!=list.end(); ++i) {
+		disease_list.push_back(*i);
+
+		std::vector<Range> param_ranges;
+		const int param_count = i->paramCount();
+		for (int param_no=0; param_no<param_count; ++param_no)
+			param_ranges.push_back(Range(QString::number(i->parameterValue(param_no), 'f')));
+		disease_param_ranges.push_back(param_ranges);
+	}
+	disease_index = QModelIndex();
+	endResetModel();
+}
+
+void DiseaseModel::setDiseases(const DiseaseList &list, std::vector<std::vector<Range> >param_ranges)
 {
 	beginResetModel();
 	disease_list.clear();
 	for (DiseaseList::const_iterator i=list.begin(); i!=list.end(); ++i)
 		disease_list.push_back(*i);
+	disease_param_ranges = param_ranges;
 	disease_index = QModelIndex();
 	endResetModel();
 }
@@ -289,6 +339,62 @@ void DiseaseModel::setSlewParameter(const Disease &d, int param_no)
 	setCompromiseModelSlewParameter(index(param_no, 0, index(disease_no, 0, root_node)));
 }
 
+void DiseaseModel::load(QSqlDatabase db)
+{
+	QSqlQuery query(db);
+	query.exec("SELECT version FROM disease_model_version");
+
+	if (!query.next())
+		throw std::runtime_error("Corrupted database file - cannot read");
+
+	const int version = query.value(0).toInt();
+	switch (version) {
+	case 1:
+		loadVersion1Db(db);
+		break;
+	default:
+		throw std::runtime_error("Invalid database file - cannot read");
+	}
+}
+
+void DiseaseModel::save(QSqlDatabase db) const
+{
+	initDb(db);
+
+	// Save diseases and its parameters
+	db.exec("DELETE FROM disease_model_params");
+	db.exec("DELETE FROM disease_model");
+
+	const int disease_count = disease_list.size();
+	QSqlQuery param_query(db), disease_query(db);
+	param_query.prepare("INSERT INTO disease_model_params "
+	                    "(disease_id, param_no, selected_param, param_range) "
+	                    "VALUES (?,?,?,?)");
+	disease_query.prepare("INSERT INTO disease_model (disease_id, disease_script) VALUES (?,?)");
+
+	for (int disease_no=0; disease_no<disease_count; ++disease_no) {
+		const std::vector<Range> &param_ranges = disease_param_ranges.at(disease_no);
+		const int param_count = param_ranges.size();
+
+		disease_query.addBindValue(disease_no);
+		disease_query.addBindValue(disease_list.at(disease_no).script());
+		if (!disease_query.exec())
+			throw std::runtime_error(disease_query.lastError().text().toUtf8().constData());
+
+		for (int param_no=0; param_no<param_count; ++param_no) {
+			const bool is_slew_param = diseaseId(disease_no, param_no) == disease_index.internalId();
+
+			param_query.addBindValue(disease_no);
+			param_query.addBindValue(param_no);
+			param_query.addBindValue(is_slew_param);
+			param_query.addBindValue(param_ranges.at(param_no).toString());
+			if (!param_query.exec())
+				throw std::runtime_error(param_query.lastError().text().toUtf8().constData());
+		}
+	}
+
+}
+
 void DiseaseModel::setCompromiseModelSlewParameter(const QModelIndex &idx)
 {
 	if (idx.column() != 0 || diseaseParamNo(idx.internalId()) == HEADER_ID)
@@ -303,4 +409,79 @@ void DiseaseModel::setCompromiseModelSlewParameter(const QModelIndex &idx)
 		emit dataChanged(old_disease_index,
 		                 index(old_disease_index.row(), 1, old_disease_index.parent()));
 	emit dataChanged(idx, index(idx.row(), 1, idx.parent()));
+}
+
+void DiseaseModel::initDb(QSqlDatabase db) const
+{
+	const int cur_version = 1;
+	QStringList db_sql[cur_version] =
+	{
+	        QStringList() <<
+	        "CREATE TABLE disease_model_params (disease_id INTEGER NOT NULL, "
+	             "param_no INTEGER NOT NULL, selected_param INTEGER NOT NULL, "
+	             "param_range NOT NULL)" <<
+	        "CREATE TABLE disease_model (disease_id INTEGER NOT NULL, disease_script NOT NULL)" <<
+	        "CREATE TABLE disease_model_version (version INTEGER NOT NULL)" <<
+	        "INSERT INTO disease_model_version VALUES(1)"
+	};
+
+	QSqlQuery q = db.exec("SELECT version FROM disease_model_version");
+	int ver = 0;
+	if (q.next())
+		ver = q.value(0).toInt();
+
+	while (ver < cur_version) {
+		foreach (QString query, db_sql[ver]) {
+			db.exec(query);
+
+			if (db.lastError().type() != QSqlError::NoError)
+				throw std::runtime_error(db.lastError().text().toUtf8().constData());
+		}
+
+		ver++;
+	}
+}
+
+void DiseaseModel::loadVersion1Db(QSqlDatabase db)
+{
+	beginResetModel();
+	disease_list.clear();
+	disease_param_ranges.clear();
+	disease_index = QModelIndex();
+
+	QSqlQuery disease_query(db), param_query(db);
+	if (!disease_query.exec("SELECT disease_id, disease_script FROM disease_model ORDER BY disease_id ASC"))
+		throw std::runtime_error(disease_query.lastError().text().toUtf8().constData());
+	param_query.prepare("SELECT selected_param, param_range FROM disease_model_params WHERE disease_id=? ORDER BY param_no ASC");
+
+	while (disease_query.next()) {
+		const int disease_id = disease_query.value(0).toInt();
+		Disease disease(disease_query.value(1).toString());
+		std::vector<Range> params;
+
+		param_query.addBindValue(disease_id);
+		if (!param_query.exec())
+			throw std::runtime_error(disease_query.lastError().text().toUtf8().constData());
+
+		while(param_query.next()) {
+			if (params.size() >= (size_t)disease.paramCount())
+				throw std::runtime_error("Too many disease parameters. Modified database.");
+
+			Range r(param_query.value(1).toString());
+			if (!r.isValid())
+				throw std::runtime_error("Invalid parameter range in saved DB");
+
+			if (param_query.value(0).toBool())
+				disease_index = createIndex(params.size(), 0, diseaseId(disease_id, params.size()));
+			params.push_back(r);
+		}
+
+		if (params.size() != (size_t)disease.paramCount())
+			throw std::runtime_error("Number of disease parameters too small in saved file");
+
+		disease_list.push_back(disease);
+		disease_param_ranges.push_back(params);
+	}
+
+	endResetModel();
 }

clickablelineedit.cpp
@@ -20,7 +20,9 @@
 #include "clickablelineedit.h"
 #include <QContextMenuEvent>
 #include <QMenu>
+#include "rangestepdlg.h"
 
+const char range_property[] = "range";
 
 ClickableLineEdit::ClickableLineEdit(QWidget *parent)
         : QLineEdit(parent)
@@ -38,9 +40,11 @@ void ClickableLineEdit::contextMenuEvent(QContextMenuEvent *e)
 {
 	QMenu *menu = createStandardContextMenu();
 	QList<QAction*> ac = menu->actions();
-	ac = actions() + ac;
+	// ac = actions() + ac;
+	QAction *range_ac = menu->addAction("&Set Range", this, SLOT(setRange()));
 	menu->addActions(ac);
-	menu->setDefaultAction(ac.first());
+	menu->setDefaultAction(range_ac);
+	range_ac->setDisabled(property(range_property).isNull());
 	menu->popup(mapToGlobal(e->pos()));
 }
 
@@ -54,4 +58,17 @@ void ClickableLineEdit::focusOutEvent(QFocusEvent *e)
 {
 	emit focusChange(false);
 	QLineEdit::focusOutEvent(e);
+}
+
+void ClickableLineEdit::setRange()
+{
+	Range range(property(range_property).toString());
+	RangeStepDlg dlg(range, this);
+	dlg.setRange(Range(text()));
+
+	if (dlg.exec() == QDialog::Accepted) {
+		range = dlg.range();
+		setText(range.toString());
+		setReadOnly(range.sequence().size() > 1);
+	}
 }

clickablelineedit.h
@@ -19,6 +19,7 @@
 
 #include <QLineEdit>
 
+extern const char range_property[];
 
 class ClickableLineEdit : public QLineEdit
 {
@@ -32,6 +33,9 @@ protected:
 	virtual void contextMenuEvent(QContextMenuEvent *);
 	virtual void focusInEvent(QFocusEvent *);
 	virtual void focusOutEvent(QFocusEvent *);
+
+protected slots:
+	void setRange();
 
 signals:
 	void focusChange(bool in_focus);

diseasemodel.h
@@ -49,19 +49,29 @@ public:
 	void clear(); // remove all diseases
 
 	DiseaseList diseases() const;
+	std::vector<std::vector<Range> > diseaseParamRanges() const;
 	void setDiseases(const DiseaseList &list);
+	void setDiseases(const DiseaseList &list, std::vector<std::vector<Range> > param_ranges);
 
 	// slew parameter for compromise model
 	bool hasSlewParameter() const { return disease_index.isValid(); }
 	Disease slewParameter(int &param_no) const;
 	void setSlewParameter(const Disease &, int param_no);
 
+	void load(QSqlDatabase db);
+	void save(QSqlDatabase db) const;
+
 public slots:
 	/* called when user double clicks on a disease parameter in disease_view */
 	void setCompromiseModelSlewParameter(const QModelIndex &index);
 
+protected:
+	void initDb(QSqlDatabase db) const;
+	void loadVersion1Db(QSqlDatabase db);
+
 private:
 	std::vector<Disease> disease_list;
+	std::vector<std::vector<Range> > disease_param_ranges;
 	QModelIndex root_node, disease_index;
 
 signals:

diseaseparamdelegate.cpp
@@ -18,13 +18,19 @@
  */
 
 #include <QDoubleSpinBox>
+#include <QItemEditorCreatorBase>
+#include "clickablelineedit.h"
 #include "diseasemodel.h"
 #include "diseaseparamdelegate.h"
 
 DiseaseParamDelegate::DiseaseParamDelegate(QObject *parent)
         : QStyledItemDelegate(parent)
 {
-
+	QItemEditorFactory *f = new QItemEditorFactory();
+	QItemEditorCreatorBase *base_edit = new QStandardItemEditorCreator<ClickableLineEdit>();
+	f->registerEditor(QVariant::Double, base_edit);
+	f->registerEditor(QVariant::String, base_edit);
+	setItemEditorFactory(f);
 }
 
 QWidget* DiseaseParamDelegate::createEditor(QWidget *parent,
@@ -32,6 +38,12 @@ QWidget* DiseaseParamDelegate::createEditor(QWidget *parent,
                                             const QModelIndex &index) const
 {
 	QWidget *editor = QStyledItemDelegate::createEditor(parent, option, index);
+
+	// set range property for Range control
+	Range range("0");
+	range.setMin(index.data(DiseaseModel::RangeMin).toDouble());
+	range.setMax(index.data(DiseaseModel::RangeMax).toDouble());
+	editor->setProperty(range_property, range.toString());
 
 	QDoubleSpinBox *spin_box = qobject_cast<QDoubleSpinBox*>(editor);
 	if (spin_box) {

mainwindow.cpp
@@ -30,7 +30,6 @@
 #include "model/compromisemodel.h"
 #include "opencldlg.h"
 #include "opencl.h"
-#include "rangestepdlg.h"
 #include "wizard.h"
 
 #include <QActionGroup>
@@ -54,7 +53,6 @@
 #endif
 #include <errno.h>
 
-static const char range_property[] = "range";
 static const QLatin1String db_name("modeldb");
 
 
@@ -142,29 +140,6 @@ MainWindow::MainWindow(QWidget *parent)
 	ui->CO->setProperty(range_property, "0 to 25;0.1");
 	ui->PAPm->setProperty(range_property, "0 to 120;0.1");
 
-	// range actions for edit controls
-	QList<QLineEdit*> editors;
-
-	editors << ui->patHt << ui->patWt << ui->lungHt << ui->MV << ui->CL
-	        << ui->Pal << ui->Ppl << ui->LAP << ui->CO << ui->PAPm;
-	foreach (QLineEdit *e, editors) {
-		QList<QAction*> a_list;
-
-		QAction *ac = new QAction("&Set Range", e);
-		connect(ac, SIGNAL(triggered()), SLOT(editRangeControl()));
-		a_list << ac;
-
-		ac = new QAction(e);
-		ac->setSeparator(true);
-		a_list << ac;
-
-		QList<QAction*> curr_actions = e->actions();
-		if (curr_actions.isEmpty())
-			e->insertActions(0, a_list);
-		else
-			e->insertActions(curr_actions.first(), a_list);
-	}
-
 	// on_actionModelWizard_triggered();
 	ui->diseaseView->hide();
 	ui->diseaseViewLabel->hide();
@@ -235,17 +210,25 @@ void MainWindow::load(const QString &filename)
 		QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", db_name);
 		db.setDatabaseName(filename);
 
-		if (db.open() && baseline->load(db, 0, &dlg)) {
-			if (!model->load(db, 1, &dlg))
-				*model = *baseline;
-			save_filename = filename;
-			setupNewModelScene();
-			updateInputsOutputs();
-			updateTitleFilename();
+		try {
+			// TODO: move to exception based error detection only
+			if (db.open() && baseline->load(db, 0, &dlg)) {
+				if (!model->load(db, 1, &dlg))
+					*model = *baseline;
+				save_filename = filename;
+				setupNewModelScene();
+				updateInputsOutputs();
+				disease_model->load(db);
+				updateTitleFilename();
+			}
+			else
+				throw std::runtime_error("Cannot load file");
 		}
-		else
+		catch (std::runtime_error error) {
+			qDebug() << error.what();
 			QMessageBox::critical(this, "Error loading file",
 			                      QString("An error occured trying to load \n%1.").arg(filename));
+		}
 
 		db.close();
 	}
@@ -284,16 +267,25 @@ void MainWindow::save(const QString &filename)
 		// in block so we are certain to destroy the `db` object prior to DB removal
 		QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", db_name);
 		db.setDatabaseName(fn);
-		if (db.open() &&
-		    baseline->save(db, 0, &dlg) &&
-		    model->save(db, 1, &dlg)) {
-			save_filename = fn;
+		try {
+			// TODO: move to exception based error detection only
+			if (db.open() &&
+			                baseline->save(db, 0, &dlg) &&
+			                model->save(db, 1, &dlg)) {
+				disease_model->save(db);
+				save_filename = fn;
+			}
+			else
+				save_error = true;
 		}
-		else {
-			QMessageBox::critical(this, "Error saving file",
-			                      QString("An error occurred trying to save to \n%1").arg(fn));
+		catch (std::runtime_error error) {
+			qDebug() << error.what();
 			save_error = true;
 		}
+
+		if (save_error)
+			QMessageBox::critical(this, "Error saving file",
+			                      QString("An error occurred trying to save to \n%1").arg(fn));
 
 		db.close();
 	}
@@ -361,6 +353,20 @@ void MainWindow::updateResults()
 	}
 
 	DiseaseList diseases = disease_model->diseases();
+	const std::vector<std::vector<Range> > disease_param_ranges = disease_model->diseaseParamRanges();
+	for (int disease_no=0; disease_no<disease_param_ranges.size(); ++disease_no) {
+		const std::vector<Range> &param_ranges = disease_param_ranges.at(disease_no);
+
+		for (int param_no=0; param_no<param_ranges.size(); ++param_no) {
+			Range range = param_ranges.at(param_no);
+
+			if (range.sequenceCount() > 0) {
+				Model::DataType type = Model::diseaseHybridType(disease_no, param_no);
+				data_ranges.push_back(QPair<Model::DataType, Range>(type, range));
+			}
+		}
+	}
+
 	baseline->clearDiseases();
 	for (DiseaseList::const_iterator i=diseases.begin(); i!=diseases.end(); ++i)
 		baseline->addDisease(*i);
@@ -437,7 +443,7 @@ void MainWindow::on_actionModelWizard_triggered()
 		}
 
 	for (DiseaseList::const_iterator i=diseases.begin(); i!=diseases.end(); ++i)
-		if (i->id() == -1) {
+		if (i->id() == -3) {
 			pvod_pah_disease.push_back(*i);
 			break;
 		}
@@ -953,26 +959,6 @@ void MainWindow::calculationCompleted()
 
 	connect(mres, SIGNAL(finished(int)), SLOT(multiModelWindowClosed()));
 	connect(mres, SIGNAL(doubleClicked(Model*)), SLOT(modelSelected(Model*)));
-}
-
-void MainWindow::editRangeControl()
-{
-	ClickableLineEdit *edit = qobject_cast<ClickableLineEdit*>(sender());
-	QAction *ac = qobject_cast<QAction*>(sender());
-	if (ac != 0 && edit == 0)
-		edit = qobject_cast<ClickableLineEdit*>(ac->parent());
-	if (edit == 0)
-		return;
-
-	Range range(edit->property(range_property).toString());
-	RangeStepDlg dlg(range, this);
-	dlg.setRange(Range(edit->text()));
-
-	if (dlg.exec() == QDialog::Accepted) {
-		range = dlg.range();
-		edit->setText(range.toString());
-		edit->setReadOnly(range.sequence().size() > 1);
-	}
 }
 
 void MainWindow::modelSelected(Model *new_model)

mainwindow.h
@@ -103,7 +103,6 @@ protected:
 
 protected slots:
 	void calculationCompleted();
-	void editRangeControl();
 	void modelSelected(Model *model);
 	void multiModelWindowClosed();
 

model/disease.h
@@ -35,7 +35,7 @@ class Disease;
 class Model;
 struct Vessel;
 struct Parameter;
-typedef std::list<Disease> DiseaseList;
+typedef QList<Disease> DiseaseList;
 
 struct Parameter {
 	QString name, desc;

commit 958ac20eb6798991476c3f8aacdfde629d25f138

Verify if an index is valid before assuming its internalId() is valid
This is a regression from 2203406eadb3916e5d2f6e37f8875515c77d480a
where this check was erroneously removed.

diseasemodel.cpp
@@ -114,7 +114,8 @@ QVariant DiseaseModel::data(const QModelIndex &index, int role) const
 	case Qt::BackgroundRole:
 		if (diseaseParamNo(index.internalId()) == HEADER_ID)
 			return QBrush(QColor(0xa0, 0xa0, 0xff));
-		if (disease_index.internalId() == index.internalId())
+		if (disease_index.isValid() &&
+		    disease_index.internalId() == index.internalId())
 			return QBrush(QColor(0xff, 0xff, 0xa0));
 		break;
 	case Qt::DisplayRole: