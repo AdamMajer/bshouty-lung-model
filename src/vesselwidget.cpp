2464b6ed521c8d1afc960c5fc815c410b13207ac
Adam Majer <adamm@zombino.com>
2012-08-29 23:39:58 -0500

    Paint flow arrow and draw vessels in same direction as the main model
    The flow in the main model is from left to right. The integration is
    done from right to left and this was the way that the vessels were
    drawn. For consistency, draw them in the opposite direction - same as
    the main model.
    
    Added an arrow of flow direction.

--- src/vesselwidget.cpp
+++ src/vesselwidget.cpp
@@ -14,28 +14,39 @@ void VesselWidget::paintEvent(QPaintEvent *ev)
 	p.setClipRegion(ev->region());
 	p.eraseRect(ev->rect());
 
-	double scaling_x = widget_rect.width() / (double)data_width / 1.25;
-	double scaling_y = widget_rect.height() / (3*y_size);
+	const int w = widget_rect.width();
+	const int h = widget_rect.height();
+	double scaling_x = w / (double)data_width / 1.25;
+	double scaling_y = h / (3*y_size);
 
-	int idx = std::max(0, static_cast<int>(ev->rect().left() / scaling_x));
-	int end_idx = std::min(data_width, static_cast<int>(ev->rect().right() / scaling_x)+1);
+	int idx = std::max(0, static_cast<int>((w-ev->rect().right()-w/8) / scaling_x));
+	int end_idx = std::min(data_width, static_cast<int>((w-ev->rect().left()-w/8) / scaling_x)+1);
 
 	QPainterPath top_path, bottom_path;
-
-	top_path.moveTo(idx*scaling_x + widget_rect.width()/8.0,
-	                widget_rect.height()/2 - vessel_r[idx]*scaling_y);
-	bottom_path.moveTo(idx*scaling_x + widget_rect.width()/8.0,
+	top_path.moveTo(w - (idx*scaling_x + w/8.0),
+	                h/2 - vessel_r[idx]*scaling_y);
+	bottom_path.moveTo(w - (idx*scaling_x + w/8.0),
 	                   widget_rect.height()/2 + vessel_r[idx]*scaling_y);
 
 	while (++idx < end_idx) {
-		top_path.lineTo(idx*scaling_x + widget_rect.width()/8.0,
-		                widget_rect.height()/2 - vessel_r[idx]*scaling_y);
-		bottom_path.lineTo(idx*scaling_x + widget_rect.width()/8.0,
-		                   widget_rect.height()/2 + vessel_r[idx]*scaling_y);
+		top_path.lineTo(w - (idx*scaling_x + w/8.0),
+		                h/2 - vessel_r[idx]*scaling_y);
+		bottom_path.lineTo(w - (idx*scaling_x + w/8.0),
+		                   h/2 + vessel_r[idx]*scaling_y);
 	}
 
 	p.setPen(QPen(Qt::red, 2, Qt::SolidLine, Qt::RoundCap));
 	p.setRenderHint(QPainter::Antialiasing);
 	p.drawPath(top_path);
 	p.drawPath(bottom_path);
+
+	// flow direction arrow
+	p.drawLine(3*w/30, h/2, 10*w/30, h/2);
+	p.drawLine(8*w/30, 9*h/20, 10*w/30, h/2);
+	p.drawLine(8*w/30, 11*h/20, 10*w/30, h/2);
+
+	QFont font = p.font();
+	font.setPointSizeF(font.pointSizeF() * (h/10) / p.fontMetrics().height());
+	p.setFont(font);
+	p.drawText(12*w/30, h/2 + p.fontMetrics().ascent()/2, QLatin1String("Flow"));
 }
