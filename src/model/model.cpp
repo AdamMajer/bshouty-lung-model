AdamMajer/bshouty-lung-model

commit 8b5c9268477c795ea0c7ecf3734745e709638d08

Calibration values are stored pre-scaled, adjust read accordingly
Some calibration values, like Hct, are stored as percentage, or using
the same scaling as Model::getResult() and Model::setData()
functions. The Model::calibrationValue() function should scale these
stored values back down to raw values used in the model itself.

Also, moved Model::calibrationValue() function from public: to
protected: namespace.

src/model/model.cpp
@@ -1024,9 +1024,20 @@ double Model::calibrationValue(DataType type)
 		bool fOK;
 		double ret_value = ret.toDouble(&fOK);
 
-		if (fOK)
-			return ret_value;
+		if (fOK) {
+			/* NOTE: Stored calibration values are pre-scaled */
+			switch (type) {
+			case Model::Vrv_value:
+			case Model::Vfrc_value:
+			case Model::Vtlc_value:
+			case Model::Hct_value:
+				return ret_value / 100.0;
+			default:
+				return ret_value;
+			}
+		}
 	}
+
 
 	switch (type) {
 	case Model::Tlrns_value:
@@ -1053,7 +1064,7 @@ double Model::calibrationValue(DataType type)
 		return -5.0;
 
 	case Model::Hct_value:
-		return 0.45;
+		return 0.45 * 100.0;
 	case Model::PA_EVL_value:
 	case Model::PV_EVL_value:
 		return 5.0;

src/model/model.h
@@ -216,9 +216,10 @@ public:
 	double getKrc();
 
 	static QString calibrationPath(DataType type);
-	static double calibrationValue(DataType);
 
 protected:
+	static double calibrationValue(DataType);
+
 	void getParameters();
 	// void getKz();
 	void vascPress();

commit 1c0a5a19b4569a7421b737a21d944e70578b7ab8

Model::calibrationValues() should return unscaled values
Regression from 8b5c9268477c795ea0c7ecf3734745e709638d08

src/model/model.cpp
@@ -1064,7 +1064,7 @@ double Model::calibrationValue(DataType type)
 		return -5.0;
 
 	case Model::Hct_value:
-		return 0.45 * 100.0;
+		return 0.45;
 	case Model::PA_EVL_value:
 	case Model::PV_EVL_value:
 		return 5.0;