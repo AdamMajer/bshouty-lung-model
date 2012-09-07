AdamMajer/bshouty-lung-model

commit ff97aa7c18f892efb19f8cb0dcb15ee34cb60604

Add vessel profile dialog and change artery (D/D0)(Ptp) function
The linear nature of the D/D0 relationship for arteries is not
possible - vessels to do expand to infinity. So adapt a function that
looks like the asymptotic vein function but expand the "linear" region
to Ptp of 70 as per the linear relationship. Linear relationship was
measured to be linear when Ptp is in 20-55 range. This will require
further review.

Vessel shapes are now drawn in the vessel detail window along with all
the calculated parameters using raw units.

src/model/integrationhelper/cpuhelper.cpp
@@ -21,6 +21,7 @@
 #include "common.h"
 #include <limits>
 #include "cpuhelper.h"
+#include <vector>
 
 
 extern const double K1;
@@ -54,8 +55,8 @@ double CpuIntegrationHelper::integrate()
 	for (int i=0; i<n_elements; ++i) {
 		v[i] = i;
 
-		futures.push_back(QtConcurrent::run(this, &CpuIntegrationHelper::integrateArtery, v[i]));
-		futures.push_back(QtConcurrent::run(this, &CpuIntegrationHelper::integrateVein, v[i]));
+		futures.push_back(QtConcurrent::run(this, &CpuIntegrationHelper::integrateVessel, Vessel::Artery, v[i], (std::vector<double>*)0));
+		futures.push_back(QtConcurrent::run(this, &CpuIntegrationHelper::integrateVessel, Vessel::Vein, v[i], (std::vector<double>*)0));
 	}
 
 	double max_deviation = 0;
@@ -67,8 +68,21 @@ double CpuIntegrationHelper::integrate()
 	return max_deviation;
 }
 
+void CpuIntegrationHelper::integrateWithDimentions(Vessel::Type t,
+                                                   int gen,
+                                                   int idx,
+                                                   std::vector<double> &calc_dim)
+{
+	calc_dim.clear();
+	calc_dim.reserve(nSums);
+	integrateVessel(t, index(gen, idx), &calc_dim);
+}
+
 double CpuIntegrationHelper::integrateArtery(int vessel_index)
 {
+	return integrateVessel(Vessel::Artery, vessel_index, 0);
+
+
 	Vessel & art = arteries()[vessel_index];
 
 	const double hct = Hct();
@@ -130,7 +144,7 @@ double CpuIntegrationHelper::integrateArtery(int vessel_index)
 	P = P + art.flow * Rs;
 	Rtot += Rs;
 
-	double D;
+	double D=0.0;
 	for( int j=1; j<nSums; j++ ){
 
 		Ptm = 1.35951002636 * ( P - art.tone ) - art.GP;
@@ -166,7 +180,15 @@ double CpuIntegrationHelper::integrateArtery(int vessel_index)
 
 double CpuIntegrationHelper::integrateVein(int vessel_index)
 {
-	Vessel & vein = veins()[vessel_index];
+	return integrateVessel(Vessel::Vein, vessel_index, NULL);
+}
+
+double CpuIntegrationHelper::integrateVessel(Vessel::Type type,
+                                             int vessel_index,
+                                             std::vector<double> *calc_dim)
+{
+	/* NOTE: calc_dim is assumed empty, if supplied */
+	Vessel & vein = (type == Vessel::Artery) ? arteries()[vessel_index] : veins()[vessel_index];
 
 	const double hct = Hct();
 	double Rtot = 0.0;
@@ -177,6 +199,7 @@ double CpuIntegrationHelper::integrateVein(int vessel_index)
 	if (isnan(P))
 		return 0.0;
 
+	double scaling_factor = (type == Vessel::Artery) ? 2.0 : 1.0;
 	double Ptm = 1.35951002636 * ( P - vein.tone ) - vein.GP;
 	double Rs;
 	double D_integral = 0.0;
@@ -188,6 +211,9 @@ double CpuIntegrationHelper::integrateVein(int vessel_index)
 	if (vein.D < 0.1) {
 		vein.D_calc = vein.D = 0.0;
 		vein.Dmax = vein.Dmin = vein.D;
+
+		if (calc_dim)
+			std::fill_n(calc_dim->begin(), nSums, 0.0);
 
 		vein.viscosity_factor = std::numeric_limits<double>::infinity();
 		vein.volume = 0;
@@ -212,19 +238,22 @@ double CpuIntegrationHelper::integrateVein(int vessel_index)
 		Rs = -Ptm/( 1.35951002636 * vein.flow ); // Starling Resistor
 	}
 	else {
-		const double inv_A = (1.0 + vein.b * exp( vein.c * Ptm )) / 0.99936058722097668220;
+		const double inv_A = (1.0 + vein.b * exp( vein.c * Ptm )) / 0.99936058722097668220 / scaling_factor;
 		vein.Dmin = vein.D / sqrt(inv_A);
 		const double vf = viscosityFactor(vein.Dmin, hct);
 		Rs = 128*Kr/M_PI * vf * dL / sqr(sqr(vein.Dmin)) * vein.vessel_ratio;
 		vein.viscosity_factor += vf;
 		vein.volume += M_PI/4.0 * sqr(vein.Dmin) * dL;
+
+		if (calc_dim)
+			calc_dim->push_back(vein.Dmin);
 	}
 
 	D_integral = vein.Dmin;
 	P = P + vein.flow * Rs;
 	Rtot = Rs;
 
-	double D;
+	double D=0.0;
 	for( int j=1; j<nSums; j++ ) {
 		Ptm = 1.35951002636 * ( P - vein.tone ) - vein.GP;
 
@@ -235,10 +264,13 @@ double CpuIntegrationHelper::integrateVein(int vessel_index)
 			                vein.perivascular_press_b * exp( vein.perivascular_press_c * ( Ptm - vein.Ppl ));
 
 		// corrected to Ptp=0, Ptm=35 cmH2O
-		const double inv_A = (1.0 + vein.b * exp( vein.c * Ptm )) / 0.99936058722097668220;
+		const double inv_A = (1.0 + vein.b * exp( vein.c * Ptm )) / 0.99936058722097668220 / scaling_factor;
 		D = vein.D / sqrt(inv_A);
 		D_integral += D;
 		const double vf = viscosityFactor(D, hct);
+
+		if (calc_dim)
+			calc_dim->push_back(D);
 
 		Rs = 128*Kr/M_PI * vf * dL / sqr(sqr(D)) * vein.vessel_ratio;
 		vein.viscosity_factor += vf;

src/mainwindow.cpp
@@ -34,6 +34,8 @@
 #include "overlaymapwidget.h"
 #include "wizard.h"
 
+#include "model/integrationhelper/cpuhelper.h"
+
 #include <QActionGroup>
 #include <QCheckBox>
 #include <QFileDialog>
@@ -812,18 +814,22 @@ void MainWindow::setOverlayType(QAction *ac)
 
 void MainWindow::modifyArtery(int gen, int idx)
 {
+	std::vector<double> dims;
+	CpuIntegrationHelper(model).integrateWithDimentions(Vessel::Artery, gen, idx, dims);
 	Vessel v(baseline->artery(gen, idx));
-
-	if (modifyVessel(VesselDlg::Artery, v))
+	VesselDlg dlg(Vessel::Artery, v, model->artery(gen,idx), dims, isVesselPtpReadOnly(), this);
+	if (dlg.exec() == QDialog::Accepted)
 		baseline->setArtery(gen, idx, v);
 }
 
 void MainWindow::modifyVein(int gen, int idx)
 {
+	std::vector<double> dims;
+	CpuIntegrationHelper(model).integrateWithDimentions(Vessel::Vein, gen, idx, dims);
 	Vessel v(baseline->vein(gen, idx));
-
-	if (modifyVessel(VesselDlg::Vein, v))
-		baseline->setVein(gen, idx, v);
+	VesselDlg dlg(Vessel::Vein, v, model->vein(gen,idx), dims, isVesselPtpReadOnly(), this);
+	if (dlg.exec() == QDialog::Accepted)
+		baseline->setArtery(gen, idx, v);
 }
 
 void MainWindow::modifyCap(int idx)
@@ -832,12 +838,6 @@ void MainWindow::modifyCap(int idx)
 	CapillaryDlg dlg(c, this);
 	if (dlg.exec() == QDialog::Accepted)
 		baseline->setCapillary(idx, c);
-}
-
-bool MainWindow::modifyVessel(VesselDlg::VesselType type, Vessel &v)
-{
-	VesselDlg dlg(type, v, isVesselPtpReadOnly(), this);
-	return dlg.exec() == QDialog::Accepted;
 }
 
 void MainWindow::transducerPositionChanged(Model::Transducer tr)

src/mainwindow.h
@@ -85,7 +85,6 @@ public slots:
 	void modifyArtery(int gen, int idx);
 	void modifyVein(int gen, int idx);
 	void modifyCap(int idx);
-	bool modifyVessel(VesselDlg::VesselType, Vessel &);
 
 	void transducerPositionChanged(Model::Transducer tr);
 

src/model/integrationhelper/abstracthelper.h
@@ -43,6 +43,7 @@ protected:
 	Vessel* veins() { return model->veins; }
 	int nElements() const { return model->nElements(); }
 	int nOutsideElements() const { return model->nOutsideElements(); }
+	int index(int gen, int idx) const { return model->startIndex(gen)+idx; }
 	double LAP() const { return model->LAP; }
 	double Hct() const { return model->Hct; }
 

src/model/integrationhelper/cpuhelper.h
@@ -26,7 +26,15 @@ public:
 	CpuIntegrationHelper(Model *model);
 
 	virtual double integrate();
+	void integrateWithDimentions(Vessel::Type t,
+	                             int gen, int idx,
+	                             std::vector<double> &calc_dim);
+
 protected:
 	double integrateArtery(int vessel_index);
 	double integrateVein(int vessel_index);
+
+	double integrateVessel(Vessel::Type type,
+	                       int vessel_index,
+	                       std::vector<double> *calc_dim);
 };

src/model/model.cpp
@@ -1058,12 +1058,12 @@ double Model::calibrationValue(DataType type)
 	case Model::PV_EVL_value:
 		return 5.0;
 	case Model::PA_Diam_value:
-		return 2.44701828402914;
+		return 3.115307890;
 	case Model::PV_Diam_value:
-		return 1.68227300704001;
+		return 1.681263294;
 
 	case Model::Krc:
-		return 3161.06957165224;
+		return 3179.855937396;
 
 	case Model::Ptp_value:
 	case Model::PAP_value:
@@ -1297,9 +1297,9 @@ void Model::initVesselBaselineCharacteristics()
 			continue;
 
 		// correcting area to Ptp=0, Ptm=35 cmH2O
-		arteries[i].a = 0.2419 / 1.2045;
-		arteries[i].b = 0.0275 / 1.2045;
-		arteries[i].c = 0;
+		//arteries[i].a = 0.2419 / 1.2045;
+		arteries[i].b = 8.95866; //0.0275 / 1.2045;
+		arteries[i].c = -0.060544062342;
 		arteries[i].tone = 0;
 
 		arteries[i].vessel_ratio = static_cast<double>(nElements(gen_no(i))) /

src/src.pri
@@ -23,6 +23,7 @@ SOURCES += \
 	$${SRC_DIR}/transducerview.cpp \
 	$${SRC_DIR}/vesseldlg.cpp \
 	$${SRC_DIR}/vesselview.cpp \
+	$${SRC_DIR}/vesselwidget.cpp \
 	$${SRC_DIR}/vesselconnectionview.cpp \
 	$${SRC_DIR}/wizard.cpp
 
@@ -50,6 +51,7 @@ HEADERS += \
 	$${SRC_DIR}/transducerview.h \
 	$${SRC_DIR}/vesseldlg.h \
 	$${SRC_DIR}/vesselview.h \
+	$${SRC_DIR}/vesselwidget.h \
 	$${SRC_DIR}/vesselconnectionview.h \
 	$${SRC_DIR}/wizard.h
 

src/vesseldlg.cpp
@@ -23,7 +23,12 @@
 #include <QMap>
 #include <QRegExpValidator>
 
-VesselDlg::VesselDlg(VesselType type, Vessel &vessel, bool PtpGP_RO, QWidget *parent)
+VesselDlg::VesselDlg(Vessel::Type type,
+                     Vessel &vessel,
+                     const Vessel &calc_values,
+                     const std::vector<double> &vessel_dims,
+                     bool PtpGP_RO,
+                     QWidget *parent)
         :QDialog(parent), v(vessel)
 {
 	ui = new Ui::VesselDlg;
@@ -65,19 +70,42 @@ VesselDlg::VesselDlg(VesselType type, Vessel &vessel, bool PtpGP_RO, QWidget *pa
 //	ui->cl->setToolTip("<p>Patient lung compliance in L/cm H2O</p><p>Changing value here will override model-wide C<sub>L</sub> value.</p><p>Range: 0.0001 - 1.0</p>");
 
 	switch (type) {
-	case Artery:
-		ui->a->setToolTip("<p>Parameter that controls arterial vessels passive characteristics (intercept) based on a linear relationship between vessel cross-sectional area (Area) and transmural pressure (P<sub>tm</sub>) according to the following relationship:</p><p><center>Area=A+B&times;P<sub>tm</sub></p>");
-		ui->b->setToolTip("<p>Parameter that controls arterial (slope) vessels cross-sectional area passive characteristics as a function of transmural pressure (P<sub>tm</sub>) according to the following relationships:</p><p><center>Area=A+B&times;P<sub>tm</sub></center></p>");
-		ui->c->hide();
-		ui->c_label->hide();
-		break;
-	case Vein:
-		ui->b->setToolTip("<p>Parameter that controls venous vessels passive characteristics based on a curvilinear relationship between vessel cross-sectional area (Area) and transmural pressure (P<sub>tm</sub>) according to the following relationship:</p><p><center>Area=1/[1+B&times;e<sup>C&times;Ptm</sup>]</center></p>");
-		ui->c->setToolTip(ui->b->toolTip());
+	case Vessel::Artery:
+		//ui->a->setToolTip("<p>Parameter that controls arterial vessels passive characteristics (intercept) based on a linear relationship between vessel cross-sectional area (Area) and transmural pressure (P<sub>tm</sub>) according to the following relationship:</p><p><center>Area=A+B&times;P<sub>tm</sub></p>");
+		//ui->b->setToolTip("<p>Parameter that controls arterial (slope) vessels cross-sectional area passive characteristics as a function of transmural pressure (P<sub>tm</sub>) according to the following relationships:</p><p><center>Area=A+B&times;P<sub>tm</sub></center></p>");
+		//ui->c->hide();
+		//ui->c_label->hide();
+		//break;
+	case Vessel::Vein:
+		//ui->b->setToolTip("<p>Parameter that controls venous vessels passive characteristics based on a curvilinear relationship between vessel cross-sectional area (Area) and transmural pressure (P<sub>tm</sub>) according to the following relationship:</p><p><center>Area=1/[1+B&times;e<sup>C&times;Ptm</sup>]</center></p>");
+		//ui->c->setToolTip(ui->b->toolTip());
 		ui->a->hide();
 		ui->a_label->hide();
 		break;
 	}
+
+
+	// set read-only calculated values
+#define SET_DATA(val) ui->c##val->setText(doubleToString(calc_values.val))
+	SET_DATA(R);
+	SET_DATA(Dmin);
+	SET_DATA(D_calc);
+	SET_DATA(Dmax);
+	SET_DATA(volume);
+	SET_DATA(length);
+	SET_DATA(vessel_ratio);
+	SET_DATA(viscosity_factor);
+	SET_DATA(b);
+	SET_DATA(c);
+	SET_DATA(tone);
+	SET_DATA(perivascular_press_a);
+	SET_DATA(perivascular_press_b);
+	SET_DATA(perivascular_press_c);
+	SET_DATA(pressure);
+	SET_DATA(flow);
+#undef SET_DATA
+
+	ui->vessel->setDrawData(vessel_dims);
 }
 
 void VesselDlg::accept()

commit 0ae3a4bc0ce19e74ea5513338728e57a4f819d3d

Fix a regression where wrong pressures were used by artery inlets
Regression from ff97aa7c where integration routine was unified between
veins and arteries. The inlet pressure for arteries was not set
properly - it was using vein inlet pressure.

src/model/integrationhelper/cpuhelper.cpp
@@ -193,7 +193,19 @@ double CpuIntegrationHelper::integrateVessel(Vessel::Type type,
 	const double hct = Hct();
 	double Rtot = 0.0;
 	double Rin = vein.R;
-	double P = ((vessel_index == 0) ? LAP() : veins()[(vessel_index-1)/2].pressure);
+	double P; // pressure to the right (vein side) of the vessel
+
+	switch (type) {
+	case Vessel::Artery:
+		P = vein.pressure;
+		break;
+	case Vessel::Vein:
+		if (vessel_index == 0)
+			P = LAP();
+		else
+			P = veins()[(vessel_index-1)/2].pressure;
+		break;
+	}
 
 	// undefined pressure signals no flow (closed vessel(s) somewhere)
 	if (isnan(P))